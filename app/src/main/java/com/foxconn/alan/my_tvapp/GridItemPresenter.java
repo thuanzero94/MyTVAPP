994871f6ef2db49dde587b83f5efadd995ffe956
lanzero <thuanzero94@gmail.com>
2018-02-08 15:32:50 +0700

    ReCreate 'GridItemPresenter' with new class separately

---
+++ app/src/main/java/com/foxconn/alan/my_tvapp/GridItemPresenter.java
@@ -0,0 +1,47 @@
+package com.foxconn.alan.my_tvapp;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v17.leanback.widget.Presenter;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by alan on 08/02/2018.
+ */
+
+public class GridItemPresenter extends Presenter{
+    private static final String TAG = GridItemPresenter.class.getSimpleName();
+
+    private static Context mContext;
+    private static final int GRID_ITEM_WIDTH = 300;
+    private static final int GRID_ITEM_HEIGHT = 200;
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent) {
+        Log.d(TAG, "onCreateViewHolder");
+        mContext=parent.getContext();
+
+        TextView view = new TextView(parent.getContext());
+        view.setLayoutParams(new ViewGroup.LayoutParams(GRID_ITEM_WIDTH, GRID_ITEM_HEIGHT));
+        view.setFocusable(true);
+        view.setFocusableInTouchMode(true);
+        view.setBackgroundColor(mContext.getResources().getColor(R.color.default_background));
+        view.setTextColor(Color.WHITE);
+        view.setGravity(Gravity.CENTER);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, Object item) {
+        Log.d(TAG, "onBindViewHolder");
+        ((TextView) viewHolder.view).setText((String) item);
+    }
+
+    @Override
+    public void onUnbindViewHolder(ViewHolder viewHolder) {
+
+    }
+}
--- app/src/main/java/com/foxconn/alan/my_tvapp/MainFragment.java
+++ app/src/main/java/com/foxconn/alan/my_tvapp/MainFragment.java
@@ -1,17 +1,12 @@
 package com.foxconn.alan.my_tvapp;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v17.leanback.app.BrowseFragment;
 import android.support.v17.leanback.widget.ArrayObjectAdapter;
 import android.support.v17.leanback.widget.HeaderItem;
 import android.support.v17.leanback.widget.ListRow;
 import android.support.v17.leanback.widget.ListRowPresenter;
-import android.support.v17.leanback.widget.Presenter;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * Created by alan on 02/02/2018.
@@ -21,8 +16,6 @@ public class MainFragment extends BrowseFragment {
     private static final String TAG = MainFragment.class.getSimpleName();
 
     private ArrayObjectAdapter mRowsAdapter;
-    private static final int GRID_ITEM_WIDTH = 300;
-    private static final int GRID_ITEM_HEIGHT = 200;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
@@ -34,37 +27,13 @@ public class MainFragment extends BrowseFragment {
     }
 
     private void setupElements(){
-        //setBadgeDrawable(getActivity().getResources().getDrawable(R.drawable.lb_action_bg));
+        setBadgeDrawable(getResources().getDrawable(R.drawable.lb_action_bg));
         setTitle("Hello Android TV");
         setHeadersState(HEADERS_ENABLED);
         setHeadersTransitionOnBackEnabled(true);
 
         setBrandColor(getResources().getColor(R.color.fastlane_BG));
         setSearchAffordanceColor(getResources().getColor(R.color.search_opaque));
-    }
-
-    private class GridItemPresenter extends Presenter{
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent) {
-            TextView view = new TextView(parent.getContext());
-            view.setLayoutParams(new ViewGroup.LayoutParams(GRID_ITEM_WIDTH, GRID_ITEM_HEIGHT));
-            view.setFocusable(true);
-            view.setFocusableInTouchMode(true);
-            view.setBackgroundColor(getResources().getColor(R.color.default_background));
-            view.setTextColor(Color.WHITE);
-            view.setGravity(Gravity.CENTER);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder viewHolder, Object item) {
-            ((TextView) viewHolder.view).setText((String) item);
-        }
-
-        @Override
-        public void onUnbindViewHolder(ViewHolder viewHolder) {
-
-        }
     }
 
     private void loadRows(){
