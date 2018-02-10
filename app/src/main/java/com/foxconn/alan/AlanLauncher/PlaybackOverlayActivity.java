8d08fa347828fa2e6ece0b7f8e1406eac4fb75e9
lanzero <thuanzero94@gmail.com>
2018-02-10 16:37:33 +0700

    TUT7, add UI for playback control

---
+++ app/src/main/java/com/foxconn/alan/AlanLauncher/PlaybackOverlayActivity.java
@@ -0,0 +1,13 @@
+package com.foxconn.alan.AlanLauncher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlaybackOverlayActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playback_overlay);
+    }
+}
---
+++ app/src/main/java/com/foxconn/alan/AlanLauncher/PlaybackOverlayFragment.java
@@ -0,0 +1,8 @@
+package com.foxconn.alan.AlanLauncher;
+
+/**
+ * Created by alan on 10/02/2018.
+ */
+
+public class PlaybackOverlayFragment {
+}
